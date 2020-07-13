51d73c283ee0e28a4f2d2a45db04a42455405b3c
Mike McGuire <MoonshotMike@gmail.com>
2020-07-13 17:02:46 -0600

    initial commit

---
+++ Hello Name/src/HelloName.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloName
+	{
+
+	public static void main(String[] args)
+		{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = userInput.nextLine();
+		System.out.println("Hello, " + name + "!");
+		}
+
+	}
